88edb31cf24030343c2381798035ab9eeddf16b8
Shreyashkar-Mishra <shreyashkarm@gmail.com>
2026-03-11 09:35:26 +0530

    Configure persistent PostgreSQL database and auto-migrations for Render

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ pip install -r requirements.txt
 # Collect static files
 python manage.py collectstatic --noinput
 
-# Run migrations (Optional: if using a real DB)
-# python manage.py migrate
+# Run migrations
+python manage.py migrate
